d522a6fcffdaa098f4675955b8a9c77e822a7d59
nathandietz <17477613+nathandietz@users.noreply.github.com>
2025-01-10 10:37:19 -0600

    Update background.js
    Changed the capture format from PNG to JPEG @ 100% quality.
    
    This creates smaller files while having no perceivable quality loss.

--- background.js
+++ background.js
@@ -1,10 +1,12 @@
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.type === 'captureTab') {
-      chrome.tabs.captureVisibleTab(null, { format: 'png' }, (dataUrl) => {
-        sendResponse(dataUrl);
-      });
-    }
+  if (request.type === 'captureTab') {      
+    
+      // Change format to JPEG & 100% quality for a smaller size file size.
+    chrome.tabs.captureVisibleTab(null, { format: 'jpeg', quality:100 }, (dataUrl) => {
+
+      sendResponse(dataUrl);
+    });
+  }
   
-    return true;
-  });
-  
+  return true;
+});
